10c67051068ceacb35a6e05466fcc9838afb06b4
Alina Olaru <olarualina01@gmail.com>
2020-06-06 17:37:48 +0300

    chain of responsability implemented

--- Design Patterns in Java Creational/My own implementations and examples/ChainOfResponsibilityDesignPattern/src/com/company/Rezident.java
+++ Design Patterns in Java Creational/My own implementations and examples/ChainOfResponsibilityDesignPattern/src/com/company/Rezident.java
@@ -1,4 +1,13 @@
 package com.company;
 
-public class Rezident {
+public class Rezident extends Handler {
+    @Override
+    public void handleRequest(Request request) {
+        if(request.getRequestType()==RequestType.RECOLTARE_ANALIZE){
+            System.out.println("Rezidentul poate RECOLTA ANALIZA");
+        }
+        else{
+            successor.handleRequest(request);
+        }
+    }
 }
