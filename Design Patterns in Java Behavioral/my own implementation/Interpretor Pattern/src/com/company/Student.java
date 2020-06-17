a02ce966e64b5f20b171a634fb483582ee786ef2
Alina Olaru <olarualina01@gmail.com>
2020-06-17 18:53:33 +0300

    Iterator pattern

--- Design Patterns in Java Behavioral/my own implementation/Interpretor Pattern/src/com/company/Student.java
+++ Design Patterns in Java Behavioral/my own implementation/Interpretor Pattern/src/com/company/Student.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class Student {
+    protected String nume;
+    protected Integer nota;
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public Student(String nume, Integer nota) {
+        this.nume = nume;
+        this.nota = nota;
+    }
+
+    public Student(Integer nota) {
+        this.nota = nota;
+    }
 }
